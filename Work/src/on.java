da0b4dbb4f2295b2984fdd423ce47a88c8a8bb4b
Evgeniy05 <e05011984@gmail.com>
2016-12-23 19:57:04 +0500

    sortirovka

--- Work/src/on.java
+++ Work/src/on.java
@@ -1,4 +1,27 @@
-
+import java.util.Arrays;
 public class on {
 String name;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int []a={5,6,1,3,4,5,44,4,4};
+		sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+
+public static void sort (int[] arr) {
+	  	for (int min=0;min<arr.length-1;min++) {
+	  		int least = min;
+	  		for (int j=min+1;j<arr.length;j++) {
+	  		    if(arr[j] < arr[least]) {
+	  				least = j;
+	  			}
+	  		}
+	 		int tmp = arr[min];
+	 		arr[min] = arr[least];
+	 		arr[least] = tmp;
+	 	}
+	 }
 }
+
