9dac971e72ae28a80720cf11ff7a73b9302384a6
Evgeniy05 <e05011984@gmail.com>
2016-12-24 08:44:43 +0500

    change

---
+++ Work/src/Books.java
@@ -0,0 +1,25 @@
+
+public class Books {
+String name;
+String Family;
+String FirstName;
+String LastName;
+String aurthor=Family+FirstName+LastName;
+/**
+ * @param name
+ * @param family
+ * @param firstName
+ * @param lastName
+ * @param aurthor
+ */
+public Books(String name, String family, String firstName, String lastName) {
+	super();
+	this.name = name;
+	Family = family;
+	FirstName = firstName;
+	LastName = lastName;
+
+}
+
+
+}
