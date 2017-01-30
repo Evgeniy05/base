651701a59d6f7f254f35ba3d6efc3c83163e6ad3
Evgeniy05 <e05011984@gmail.com>
2017-01-30 16:03:07 +0500

    change class Item. remove all not need

--- Factory/src/com/exmaples/factory/Item.java
+++ Factory/src/com/exmaples/factory/Item.java
@@ -1,94 +1,20 @@
 package com.exmaples.factory;
 
 public class Item {
-	private String name;
-	private String date;
 	private int id;
-	private long size;
-	private Supplier supplier;
 	private static int nextId = 1;
-	private Engine engine;
-	private Body body;
-	private Accessory accessory;
+
 	{
 		id = nextId;
 		nextId++;
-	}
-
-	public Item() {
-		this("Item#");
-	}
-
-	public Item(Supplier aSupplier) {
-		supplier = aSupplier;
-	}
-
-	public Item(long aSize, String aName) {
-		name = aName;
-		size = aSize;
-	}
-
-	public Item(String aName) {
-		name = aName;
-	}
-
-	public Item(Engine aEngine, Body aBody, Accessory aAccessory) {
-		engine = aEngine;
-		body = aBody;
-		accessory = aAccessory;
-		this.name = Factory.AUTO;
-	}
-
-	public Engine getEngine() {
-		return engine;
-	}
-
-	public void setEngine(Engine engine) {
-		this.engine = engine;
-	}
-
-	public Body getBody() {
-		return body;
-	}
-
-	public void setBody(Body body) {
-		this.body = body;
-	}
-
-	public Accessory getAccessory() {
-		return accessory;
-	}
-
-	public void setAccessory(Accessory accessory) {
-		this.accessory = accessory;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public Supplier getSupplier() {
-		return supplier;
-	}
-
-	public void setSize(long size) {
-		this.size = size;
-	}
-
-	public long getSize() {
-		return size;
-	}
+	public static final String BODY = "Body";
+	public static final String ENGINE = "Engine";
+	public static final String ACCESSORY = "Accessory";
 
 }
