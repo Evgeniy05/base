88af12e7f4cd16f0e8901eac7d15e6249c10f779
Evgeniy05 <e05011984@gmail.com>
2017-01-30 16:10:17 +0500

    sychronized methods

--- Factory/src/com/exmaples/factory/Storage.java
+++ Factory/src/com/exmaples/factory/Storage.java
@@ -4,24 +4,24 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Storage extends Item {
+public class Storage {
 	private ConcurrentHashMap<Integer, Item> items;
-	private Auto auto;
-	private Engine engine;
-	private Body body;
 	private Item item;
-	private Accessory accessory;
-	public int result = 0;
+	private int size;
+	private String name;
+
 	{
 		items = new ConcurrentHashMap<>();
 	}
 
-	public Storage(int aSize, String aName) {
-		super(aSize, aName);
+	public Storage(int size, String name) {
+		this.size = size;
+		this.name = name;
+
 	}
 
-	public Storage(String aName) {
-		super(aName);
+	public Storage(String name) {
+		this.name = name;
 	}
 
 	public Storage() {
@@ -32,6 +32,14 @@ public class Storage extends Item {
 		return items;
 	}
 
+	public int getSize() {
+		return size;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	public long getValue() {
 		return items.mappingCount();
 	}
@@ -40,68 +48,12 @@ public class Storage extends Item {
 		return getSize() - getValue();
 	}
 
-	public void addItem(Item item) {
-		// if (item == null) {
-		// return;
-		// }
+	public synchronized void addItem(Item item) throws InterruptedException {
+		while (getFreeValue() == 0) {
+			wait();
+		}
 		items.putIfAbsent(item.getId(), item);
-	}
-
-	public Item getItem(String name) {
-		switch (name) {
-		case Factory.ACCESSORY:
-			item = getAccessory();
-			break;
-		case Factory.AUTO:
-			item = getAuto();
-			break;
-		case Factory.BODY:
-			item = getBody();
-		case Factory.ENGINE:
-			item = getEngine();
-		}
-		return item;
-	}
-
-	public Auto getAuto() {
-		Iterator<Entry<Integer, Item>> it = items.entrySet().iterator();
-		if (it.hasNext()) {
-			auto = (Auto) it.next().getValue();
-		}
-		itemRemove(auto);
-		return auto;
-	}
-
-	@Override
-	public Body getBody() {
-		Iterator<Entry<Integer, Item>> it = items.entrySet().iterator();
-		if (it.hasNext()) {
-			body = (Body) it.next().getValue();
-		}
-		itemRemove(body);
-		return body;
-	}
-
-	@Override
-	public Engine getEngine() {
-		Iterator<Entry<Integer, Item>> it = items.entrySet().iterator();
-		if (it.hasNext()) {
-			engine = (Engine) it.next().getValue();
-		}
-		itemRemove(engine);
-		return engine;
-	}
-
-	@Override
-	public Accessory getAccessory() {
-		Iterator<Entry<Integer, Item>> it = items.entrySet().iterator();
-		if (it.hasNext()) {
-
-			accessory = (Accessory) it.next().getValue();
-		}
-
-		itemRemove(accessory);
-		return accessory;
+		notifyAll();
 	}
 
 	public void itemRemove(Item item) {
@@ -115,12 +67,20 @@ public class Storage extends Item {
 
 	}
 
-	public void itemRemove() {
-		Iterator it = items.entrySet().iterator();
-		if (it.hasNext()) {
-			Entry item = (Entry) it.next();
-			it.remove(item);
+	public synchronized Item getItem(String type) throws InterruptedException {
+		while (getValue() == 0) {
+			wait();
 		}
-	}
+		for (int key : items.keySet()) {
+			if (items.get(key).getClass().getSimpleName().equals(type)) {
+				item = items.get(key);
 
+				break;
+			}
+		}
+		itemRemove(item);
+		notifyAll();
+		return item;
+
+	}
 }
