86f09e68754c4d8acc6c3342f59df8a4b07bc6d2
Evgeniy05 <e05011984@gmail.com>
2017-01-30 15:58:59 +0500

    change (WorkSpace load Factory,load Suppliers,load all )

--- Factory/src/com/exmaples/factory/Application.java
+++ Factory/src/com/exmaples/factory/Application.java
@@ -7,13 +7,17 @@ public class Application {
 	public static void main(String[] args) throws InterruptedException, ExecutionException, InstantiationException,
 			IllegalAccessException, ClassNotFoundException {
 
-		Factory factory = new Factory();
-
+		WorkSpace work = new WorkSpace();
+		System.out.println(work.factory.storages.get(Item.ENGINE).getStorage());
+		System.out.println(work.factory.storages.get(Item.BODY).getStorage());
+		System.out.println(work.factory.storages.get(Item.ACCESSORY).getStorage());
+		System.out.println(work.factory.storages.get(Auto.AUTO).getStorage().size());
+		// work.results();
 		// ItemCreator(factory.supplierEngine));
 		// count++;
 		//
 		// // factory.supplierEngine.setFlag(false);
-		// System.out.println(result.get().getStorage().getStorage().mappingCount());
+
 		// System.out.println(factory.supplierBody.getStorage().getStorage());
 		// System.out.println(factory.getStorage().values().iterator().next().getStorage());
 
