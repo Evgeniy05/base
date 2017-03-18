f0d37f8ad1f84df142fb64ad28bb1f326d52703f
Evgeniy05 <e05011984@gmail.com>
2017-03-18 09:12:10 +0500

    do not change

--- Work/src/test/Reverse.java
+++ Work/src/test/Reverse.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Reverse {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int[] a = { 5, 6, 1, 3, 4, 5, 44, 4, 4 };
-		test.Sortirovka.sortBubl(a);
+
 		System.out.println(Arrays.toString(a));
 
 		System.out.println(Arrays.toString(reverseTwo(a)));
