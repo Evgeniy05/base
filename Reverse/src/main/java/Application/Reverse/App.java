d87a47059341ec3fa0e6b5fd9b2930c92d4b59d6
Evgeniy05 <e05011984@gmail.com>
2017-03-18 09:19:56 +0500

    Program Reverse

---
+++ Reverse/src/main/java/Application/Reverse/App.java
@@ -0,0 +1,38 @@
+package Application.Reverse;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		int[] a = { 5, 6, 1, 3, 4, 5, 44, 4, 4 };
+
+		System.out.println(Arrays.toString(a));
+
+		System.out.println(Arrays.toString(reverseTwo(a)));
+
+	}
+
+	public static int[] reverseTwo(int[] arr) {
+		int[] result = new int[arr.length];
+		for (int i = 0; i <= arr.length - 1; i++) {
+			result[i] = arr[arr.length - 1 - i];
+
+		}
+
+		return result;
+	}
+
+	public static void reverse(int[] arr) {
+		int temp = 0;
+		for (int i = 0; i < (arr.length - 1) / 2; i++) {
+			temp = arr[arr.length - 1 - i];
+			arr[arr.length - 1 - i] = arr[i];
+			arr[i] = temp;
+		}
+
+	}
+}
